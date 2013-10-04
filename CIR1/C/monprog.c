1e46fc8fee76b4aceaf0ed87b440982f9952e4c1
Brendan Abolivier <contact@brendanabolivier.com>
2013-10-04 11:34:35 +0200

    lolnope

--- CIR1/C/monprog.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-
-int main()
-{
-	printf("Hello world!\n\n");
-	int a = 12;
-	printf("La variable a a une valeur de %d.\n", a);
-	printf("Longueur d'un entier : %d octets.\n", sizeof(int));
-}
