5352c5d382174ba78122a9a10a50ab4fec099706
Brendan Abolivier <contact@brendanabolivier.com>
2013-12-19 09:11:48 +0100

    Modification : Utilisation de malloc()

--- CIR1/C/voyelles-main.c
+++ CIR1/C/voyelles-main.c
@@ -3,30 +3,35 @@
 #include <string.h>
 #define LG_MAX 30
 
-void voyelles(char *, char *);
+char * voyelles(const char *);
 
 int main() {
 	char chaine[LG_MAX+1];
-	char voyellesTab[LG_MAX+1] = "aaaaaaaaaaaaaaaaaaaaaaaa";
+	char * voyellesTab = NULL;
 	
 	printf("Merci d'entrer une chaîne de caractères (30 caractères max) : ");
 	
 	fgets(chaine, LG_MAX+2, stdin);
 	chaine[strlen(chaine)-1] = '\0';
 	
-	voyelles(chaine, voyellesTab);
+	voyellesTab = voyelles(chaine);
 	printf("%s\n", voyellesTab);
 	
 	return EXIT_SUCCESS;
 }
 
-void voyelles(char * chaine, char * tab) {
-	while(*chaine != '\0') {
+char * voyelles(const char * chaine) {
+	char * tab = malloc(strlen(chaine)+1);
+	char * ptab = tab;
+	
+	while (*chaine != '\0') {
 		if(*chaine == 'a' || *chaine == 'e' || *chaine == 'i' || *chaine == 'o' || *chaine == 'u' || *chaine == 'y') {
-			*tab = *chaine;
-			tab++;
+			*ptab = *chaine;
+			ptab++;
 		}
 		chaine++;
-		*tab = '\0';
+		*ptab = '\0';
 	}
+	
+	return tab;
 }
