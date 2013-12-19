82f4e95c8f9fe15a4b7e42afbd63a74087ac0e1f
Brendan Abolivier <contact@brendanabolivier.com>
2013-12-19 10:09:51 +0100

    Mise à jour des messages

--- CIR1/C/newtp.sh
+++ CIR1/C/newtp.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 if [ -f $1 ]
 then
-	echo "Aucun argument"
+	echo "Argument incorrect.\nCeci peut arriver lorsque l'argument saisi est le nom d'un fichier existant dans le répertoire courant, ou lorsque aucun argument n'est saisi."
 else
 	mkdir $1/
 	touch $1/$1.c
 	touch $1/$1.h
 	touch $1/fonctions.c
 	touch $1/Makefile
-	echo "$1 : fonctions.o $1.o
-        gcc fonctions.o $1.o -o $1
-fonctions.o : fonctions.c $1.h
-        gcc -c fonctions.c
-crypto.o : $1.c $1.h
-        gcc -c $1.c
-clean :
-        rm *.o $1" > $1/Makefile
+	echo "$1 : fonctions.o $1.o\n\tgcc fonctions.o $1.o -o $1\nfonctions.o : fonctions.c $1.h\n\tgcc -c fonctions.c\ncrypto.o : $1.c $1.h\n\tgcc -c $1.c\nclean :\n\trm *.o $1" > $1/Makefile
 	echo "#include \"$1.h\"\n" > $1/fonctions.c
         echo "#include \"$1.h\"\n" > $1/$1.c
-	echo "#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>\n" > $1/$1.h
+	echo "#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <errno.h>\n" > $1/$1.h
+	echo "TP $1 créé avec succès !"
 fi
