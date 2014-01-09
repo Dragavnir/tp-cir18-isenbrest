ee6ced23845f5a107cf056055dbdce1bc5673756
Brendan Abolivier <contact@brendanabolivier.com>
2014-01-09 08:23:27 +0100

    clean supprime maintenant les .gch qui peuvent gêner à la compilation

--- CIR1/C/newtp.sh
+++ CIR1/C/newtp.sh
@@ -8,7 +8,7 @@ else
 	touch $1/$1.h
 	touch $1/fonctions.c
 	touch $1/Makefile
-	echo "$1 : fonctions.o $1.o\n\tgcc fonctions.o $1.o -o $1\nfonctions.o : fonctions.c $1.h\n\tgcc -c fonctions.c\ncrypto.o : $1.c $1.h\n\tgcc -c $1.c\nclean :\n\trm *.o $1" > $1/Makefile
+	echo "$1 : fonctions.o $1.o\n\tgcc fonctions.o $1.o -o $1\nfonctions.o : fonctions.c $1.h\n\tgcc -c fonctions.c\ncrypto.o : $1.c $1.h\n\tgcc -c $1.c\nclean :\n\trm *.o *.gch $1" > $1/Makefile
 	echo "#include \"$1.h\"\n" > $1/fonctions.c
         echo "#include \"$1.h\"\n\nint main() {\n\t\n\treturn EXIT_SUCCESS;\n}" > $1/$1.c
 	echo "#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <errno.h>\n#include <math.h>" > $1/$1.h
